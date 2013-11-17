67d795e9b7562e58044fdaaa612c3cc7d32caaa5
Marcos Pérez <infommpc@gmail.com>
2013-11-17 19:01:18 +0100

    User profile page

--- spec/requests/users_pages_spec.rb
+++ spec/requests/users_pages_spec.rb
@@ -1,10 +1,22 @@
 require 'spec_helper'
 
 describe "Users Pages" do
+  
   subject { page }
+  
   describe "sign up page" do
-    before { visit signup_path}
+    before { visit signup_path }
     it { should have_content('Sign Up') }
     it { should have_title(full_title('Sign Up')) }
+  end  
+  
+  describe "Profile Page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
   end
+  
 end
+
+
