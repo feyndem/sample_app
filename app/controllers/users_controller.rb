6d9845b68a363617ac8adf04325e726bc8b0e497
Marcos Pérez <infommpc@gmail.com>
2013-11-18 20:22:15 +0100

    Chapter 7 Exercices Rest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Sign up succesful!"
+      flash[:success] = "Welcome!"
       redirect_to @user
     else
       render 'new'
