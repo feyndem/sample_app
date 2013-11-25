efed8765a82659efa34b3401a20d0106e13c30fd
Marcos Pérez <infommpc@gmail.com>
2013-11-25 20:47:10 +0100

    Edit user form

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -33,6 +33,18 @@ describe "AuthenticationPages" do
       it { should_not have_link('Sign In', href: signin_path) }
     end
     
-  end      
+  end  
+  
+  describe "with valid information" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { sign_in(user) }
+    
+    it { should have_title(user.name) }
+    it { should have_link('Profile', href: user_path(user)) }
+    it { should have_link('Settings', href: edit_user_path(user)) }
+    it { should have_link('Sign Out', href: signout_path) }
+    it { should_not have_link('Sign In', href: signin_path) }
+    
+  end    
   
 end
