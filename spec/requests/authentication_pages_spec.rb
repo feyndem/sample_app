0de389a3b020650b8916de798a4b45367a87bc36
Marcos Pérez <infommpc@gmail.com>
2013-12-21 19:23:42 +0100

    Followers/following pages

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -83,6 +83,16 @@ describe "AuthenticationPages" do
           before { visit users_path }
           it { should have_title('Sign In') }
         end
+        
+        describe "visitng the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign In') }
+        end
+        
+        describe "visitng the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign In') }
+        end        
                       
       end
       
